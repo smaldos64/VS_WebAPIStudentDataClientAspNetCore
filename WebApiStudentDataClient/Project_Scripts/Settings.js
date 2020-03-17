7633752ef51f3f1eefb28f39b042ff8bf2eb6996
smaldos64 <gudumholm34@gmail.com>
2020-03-17 17:52:01 +0100

    Omstruktureret kode så det bliver meget let at skifte mellem kommunkation op imod Asp.Net Web Api og Asp.Net Core Web Api.

--- WebApiStudentDataClient/Project_Scripts/Settings.js
+++ WebApiStudentDataClient/Project_Scripts/Settings.js
@@ -1,20 +1,47 @@
-﻿//const WEB_API_URL_StudentData = "http://localhost:62090/api/";
+﻿const WebApiTechnology = {
+    Asp_Net: 1,
+    Asp_Net_Core: 2
+};
+Object.freeze(WebApiTechnology);
 
-//const WEB_API_URL_StudentData = "http://webapistudentdata.buchwaldshave34.dk/api/";
+// FrameWorkSelector variablen herunder bruges til at bestemme, om der arbejdes op imod
+// det gamle Web Api udviklet i Asp.Net Framework eller op imod det nye Web Api udviklet
+// i Asp.Net Core Framework. Der er desværre bogle ting, der ikke er helt ens i de 2 
+// nævnte teknologier. så derfor er det nødvendig med varaiblen her til at styre rundt omkring
+// i programkoden, hvilken af de 2 teknologier, der arbejdes op imod. 
+const FrameWorkSelector = WebApiTechnology.Asp_Net_Core;
 
-// Asp.Net Core Web API herunder
+var WEB_API_URL_StudentData;
 
-const WEB_API_URL_StudentData = "https://localhost:44379/api/";
+// Asp.Net Web Api herunder. Vælg én af de angivne URL's her, hvis du arbejder op imod det gamle
+// Web API udviklet i Asp.Net Framework.
 
-//const WEB_API_URL_StudentData = "http://localhost:52525/api/";
+if (FrameWorkSelector == WebApiTechnology.Asp_Net) {
+    //WEB_API_URL_StudentData = "http://localhost:62090/api/";
 
-//1const WEB_API_URL_StudentData = "http://webapistudentdatanetcore.buchwaldshave34.dk/api/";
+    //WEB_API_URL_StudentData = "http://webapistudentdata.buchwaldshave34.dk/api/";
 
-//const WEB_API_URL_StudentData = "https://webapistudentdatanetcore.buchwaldshave34.dk/api/";
+    WEB_API_URL_StudentData = "https://webapistudentdata.buchwaldshave34.dk/api/";
+}
 
+// Asp.Net Core Web API herunder. Vælg én af de angivne URL's her, hvis du arbejder op imod det nye
+// Web API udviklet i Asp.Net Core Framework.
 
+if (FrameWorkSelector == WebApiTechnology.Asp_Net_Core) {
+    //WEB_API_URL_StudentData = "https://localhost:44379/api/";
 
+    //WEB_API_URL_StudentData = "http://localhost:52525/api/";
 
+    //WEB_API_URL_StudentData = "http://webapistudentdatanetcore.buchwaldshave34.dk/api/";
+
+    WEB_API_URL_StudentData = "https://webapistudentdatanetcore.buchwaldshave34.dk/api/";
+}
+
+// Herunder opsættes stien til alle de Controolers i Web Api'et, ,an skal bruge. Læg mærke
+// til at alting sættes op releativt til WEB_API_URL_StudentData varialen defineret herover.
+// På denne måde skal vi kun ændre ét sted (nemlig i variablen WEB_API_URL_StudentData), uanset
+// hvilket Web Api vi kommunikerer op imod. Dette kræver selvfølgelig, at Controlller navne
+// er de samme i de 2 Web Api, der kan kommunikeres op imod !!!
 const WEB_API_URL_StudentData_Course = WEB_API_URL_StudentData + "Course";
 const WEB_API_URL_StudentData_Eductaion = WEB_API_URL_StudentData + "Education";
 const WEP_API_URL_USERINFO = WEB_API_URL_StudentData + "UserInfo";
@@ -32,6 +59,27 @@ const jSonDecodingOk = 0;
 const OperationOkHigherValueThanHere = 0;
 
 var ReturnCodeAndReturnStringFromWEBApiList = [];
+
+function TrimReturnString(ReturnString) {
+    var OutString = ReturnString.toString();
+    
+    var Position = OutString.indexOf(" ");
+
+    if (Position > 0) {
+        OutString = OutString.substring(0, Position - 1);
+    }
+
+    try {
+        OutInt = parseInt(OutString);
+        return OutInt;
+        // Vi skulle gerne kunne returnere et "rent" tal !!!
+    }
+    catch (Exception) {
+        return ReturnString;
+    }
+
+    return (ReturnString);
+}
 
 function FindReturnNumberString(ReturnList, ReturnNumber) {
     ReturnNumberCounter = 0;
@@ -101,9 +149,16 @@ function GetReturnNumbersAndReturnStringFromWebAPI() {
 function DecodejSONReturnCodesAndReturnStrings(jSonData) {
     ReturnCodeAndReturnStringFromWEBApiList.splice(0, ReturnCodeAndReturnStringFromWEBApiList.length);
     $.each(jSonData, function (key, item) {
+        if (FrameWorkSelector == WebApiTechnology.Asp_Net) {
+            ReturnCodeAndReturnStringFromWEBApiList.push(new ReturnCodeAndReturnStringFromWEBApi(item.ReturnCode, item.ReturnString));
+        }
+
+        if (FrameWorkSelector == WebApiTechnology.Asp_Net_Core) {
+            ReturnCodeAndReturnStringFromWEBApiList.push(new ReturnCodeAndReturnStringFromWEBApi(item.returnCode, item.returnString));
+        }
         //ReturnCodeAndReturnStringFromWEBApiList.push(new ReturnCodeAndReturnStringFromWEBApi(item.ReturnCode, item.ReturnString));
         // Brug syntaksen herover, hvis du kommunikerer op imod det "gamle" Asp.Net Framework Web API.
-        ReturnCodeAndReturnStringFromWEBApiList.push(new ReturnCodeAndReturnStringFromWEBApi(item.returnCode, item.returnString));
+        //ReturnCodeAndReturnStringFromWEBApiList.push(new ReturnCodeAndReturnStringFromWEBApi(item.returnCode, item.returnString));
         // Brug syntaksen herover, hvis du kommunikerer op imod det "nye" Asp.Net.Core Web API.
     });
 }
